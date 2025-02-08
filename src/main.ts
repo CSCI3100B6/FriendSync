ee1d8109967d87eaa4658dd05e0aa107eb9f4dda
stevenpang <1507861093@qq.com>
2025-02-08 20:59:59 +0800

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { createApp, ref } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { Button, NavBar, Icon, Tabbar, TabbarItem, Toast} from 'vant';
+
+const app = createApp(App);
+app.use(Button);
+app.use(NavBar);
+app.use(Icon);
+app.use(Tabbar);
+app.use(TabbarItem);
+app.use(Toast);
+app.mount('#app')
+
+
