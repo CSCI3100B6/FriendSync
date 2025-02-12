9720c08fb48adeb9c4552ea00a0c3e134835da1f
Muush <2419108998@qq.com>
2025-02-12 12:13:25 +0800

    add h2 database console, user entity

---
+++ server/src/main/java/com/friendsync/server/TestController.java
@@ -0,0 +1,23 @@
+package com.friendsync.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/")
+	public String getUser(HttpServletRequest request) {
+		HttpSession session = request.getSession();
+		StringBuilder sb = new StringBuilder();
+		sb.append("Id: " + session.getId());
+		sb.append("<br>CreationTime: " + session.getCreationTime());
+		sb.append("<br>LastAccessedTime: " + session.getLastAccessedTime());
+		return sb.toString();
+	}
+}
---
+++ server/src/main/java/com/friendsync/server/User.java
@@ -0,0 +1,75 @@
+package com.friendsync.server;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long uid;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = false)
+    private String password;
+
+    @Column(nullable = false, unique = false)
+    private String name;
+
+    @Column(nullable = true, unique = true)
+    private String currentSession;
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.currentSession = null;
+    }
+
+    public Long getUid() {
+        return uid;
+    }
+
+    public void setUid(Long uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getCurrentSession() {
+        return currentSession;
+    }
+
+    public void setCurrentSession(String currentSession) {
+        this.currentSession = currentSession;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
