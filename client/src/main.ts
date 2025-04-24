57b966235b50c2fa3931d1c6d43719f6b26ea794
Josh_Wong <1155227159@link.cuhk.edu.hk>
2025-04-24 18:04:06 +0800

    update ui

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import "./style.css";
+import "./assets/form.css";
 import App from "./App.vue";
 import {
   Button,
@@ -14,7 +15,15 @@ import {
 } from "vant";
 import router from "./router";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { faUserFriends } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faUserFriends);
+
 const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+
 app.use(Button);
 app.use(NavBar);
 app.use(Icon);
