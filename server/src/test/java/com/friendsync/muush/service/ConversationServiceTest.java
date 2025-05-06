86d0b092ba852695d91e6d25ec5e2cc546cff9b6
stevenpang <1507861093@qq.com>
2025-05-06 21:18:49 +0800

    add some functions of team making.

--- server/src/test/java/com/friendsync/muush/service/ConversationServiceTest.java
+++ server/src/test/java/com/friendsync/muush/service/ConversationServiceTest.java
@@ -2,13 +2,13 @@ package com.friendsync.muush.service;
 
 import java.util.List;
 
+import com.friendsync.stevenpang.model.domain.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.friendsync.muush.repo.Conversation;
-import com.friendsync.stevenpang.model.User;
 
 import jakarta.annotation.Resource;
 
